aa26c7feeba0f75cd426c52995044c59e320ca44
Priyanka <priyankacarpenter30@gmail.com>
2023-12-11 15:09:17 +0530

    setup urls.py

--- testapp/urls.py
+++ testapp/urls.py
@@ -4,6 +4,6 @@ from django.urls import path, include
 from testapp import views
 urlpatterns = [
     path('courses', views.CourseListView.as_view()),
-    path('course_api/<int:pk>/', views.CourseDetailView.as_view())
+    path('courses/<int:pk>', views.CourseDetailView.as_view())
  
 ]
